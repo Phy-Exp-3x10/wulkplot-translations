8fa332dba5c026b8a429e988f2efd8615bab4a3a
Alyner3 <qualienhd@gmail.com>
2025-05-29 17:31:48 +0200

    Initial commit

---
+++ wulkplot_no.ts
@@ -0,0 +1,169 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO" sourcelanguage="en_US">
+<context>
+    <name>ColumnSelectionDialog</name>
+    <message>
+        <location filename="../util_qt/__generic/qt_dialogs.py" line="14"/>
+        <source>Select columns</source>
+        <translation>Velg kolonner</translation>
+    </message>
+    <message>
+        <location filename="../util_qt/__generic/qt_dialogs.py" line="24"/>
+        <source>Add</source>
+        <translation>Legg til</translation>
+    </message>
+    <message>
+        <location filename="../util_qt/__generic/qt_dialogs.py" line="25"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+</context>
+<context>
+    <name>ConsoleWindow</name>
+    <message>
+        <location filename="../util_qt/__advanced/console_window.py" line="11"/>
+        <source>Console</source>
+        <translation>Konsoll</translation>
+    </message>
+    <message>
+        <location filename="../util_qt/__advanced/console_window.py" line="15"/>
+        <source>Time</source>
+        <translation>Tid</translation>
+    </message>
+    <message>
+        <location filename="../util_qt/__advanced/console_window.py" line="15"/>
+        <source>PID</source>
+        <translation>PID</translation>
+    </message>
+    <message>
+        <location filename="../util_qt/__advanced/console_window.py" line="15"/>
+        <source>Output</source>
+        <translation>Utdata</translation>
+    </message>
+</context>
+<context>
+    <name>DataFileSelectionDialog</name>
+    <message>
+        <location filename="../util_qt/__generic/qt_dialogs.py" line="59"/>
+        <source>Add data files</source>
+        <translation>Legg til datafiler</translation>
+    </message>
+    <message>
+        <location filename="../util_qt/__generic/qt_dialogs.py" line="66"/>
+        <source>Add</source>
+        <translation>Legg til</translation>
+    </message>
+    <message>
+        <location filename="../util_qt/__generic/qt_dialogs.py" line="67"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../util_qt/__generic/qt_dialogs.py" line="75"/>
+        <source>Files</source>
+        <translation>Filer</translation>
+    </message>
+</context>
+<context>
+    <name>ExperimentGUI</name>
+    <message>
+        <location filename="../gui.py" line="76"/>
+        <source>Add files</source>
+        <translation>Legg til filer</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="81"/>
+        <source>Run script</source>
+        <translation>Kjør skript</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="87"/>
+        <source>Script parameters</source>
+        <translation>Skriptparametere</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="116"/>
+        <source>Scripts</source>
+        <translation>Skript</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="130"/>
+        <source>Script</source>
+        <translation>Skript</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="130"/>
+        <source>PID</source>
+        <translation>PID</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="130"/>
+        <source>Action</source>
+        <translation>Handling</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="214"/>
+        <source>Failed to read CSV file:</source>
+        <translation>Klarte ikke å lese den CSV-filen:</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="223"/>
+        <source>No columns selected</source>
+        <translation>Ingen kolonner valgt</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="315"/>
+        <source>builtin</source>
+        <translation>innebygd</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="383"/>
+        <source>No script selected</source>
+        <translation>Ingen skript valgt</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="402"/>
+        <source>No data files selected</source>
+        <translation>Ingen datafiler valgt</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="437"/>
+        <source>Failed to start script:</source>
+        <translation>Klarte ikke å starte skriptet:</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="474"/>
+        <source>Kill</source>
+        <translation>Drep</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="493"/>
+        <source>Failed to kill process:</source>
+        <translation>Klarte ikke å avslutte prosessen:</translation>
+    </message>
+    <message>
+        <location filename="../gui.py" line="604"/>
+        <source>Failed to load script:</source>
+        <translation>Klarte ikke å laste inn skriptet:</translation>
+    </message>
+</context>
+<context>
+    <name>SelectedFilesWidget</name>
+    <message>
+        <location filename="../util_qt/__generic/qt_widgets.py" line="13"/>
+        <source>File</source>
+        <translation>Fil</translation>
+    </message>
+    <message>
+        <location filename="../util_qt/__generic/qt_widgets.py" line="13"/>
+        <source>Columns</source>
+        <translation>Kolonner</translation>
+    </message>
+    <message>
+        <location filename="../util_qt/__generic/qt_widgets.py" line="13"/>
+        <source>Actions</source>
+        <translation>Handlinger</translation>
+    </message>
+</context>
+</TS>
---
+++ wulkplot_ru.ts
@@ -0,0 +1,169 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+  <context>
+    <name>ColumnSelectionDialog</name>
+    <message>
+      <location filename="..\util_qt\__generic\qt_dialogs.py" line="14" />
+      <source>Select columns</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\util_qt\__generic\qt_dialogs.py" line="24" />
+      <source>Add</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\util_qt\__generic\qt_dialogs.py" line="25" />
+      <source>Cancel</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>ConsoleWindow</name>
+    <message>
+      <location filename="..\util_qt\__advanced\console_window.py" line="11" />
+      <source>Console</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\util_qt\__advanced\console_window.py" line="15" />
+      <source>Time</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\util_qt\__advanced\console_window.py" line="15" />
+      <source>PID</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\util_qt\__advanced\console_window.py" line="15" />
+      <source>Output</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>DataFileSelectionDialog</name>
+    <message>
+      <location filename="..\util_qt\__generic\qt_dialogs.py" line="59" />
+      <source>Add data files</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\util_qt\__generic\qt_dialogs.py" line="66" />
+      <source>Add</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\util_qt\__generic\qt_dialogs.py" line="67" />
+      <source>Cancel</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\util_qt\__generic\qt_dialogs.py" line="75" />
+      <source>Files</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>ExperimentGUI</name>
+    <message>
+      <location filename="..\gui.py" line="76" />
+      <source>Add files</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="81" />
+      <source>Run script</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="87" />
+      <source>Script parameters</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="116" />
+      <source>Scripts</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="130" />
+      <source>Script</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="130" />
+      <source>PID</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="130" />
+      <source>Action</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="214" />
+      <source>Failed to read CSV file:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="223" />
+      <source>No columns selected</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="315" />
+      <source>builtin</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="383" />
+      <source>No script selected</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="402" />
+      <source>No data files selected</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="437" />
+      <source>Failed to start script:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="474" />
+      <source>Kill</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="493" />
+      <source>Failed to kill process:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\gui.py" line="604" />
+      <source>Failed to load script:</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>SelectedFilesWidget</name>
+    <message>
+      <location filename="..\util_qt\__generic\qt_widgets.py" line="13" />
+      <source>File</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\util_qt\__generic\qt_widgets.py" line="13" />
+      <source>Columns</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="..\util_qt\__generic\qt_widgets.py" line="13" />
+      <source>Actions</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+</TS>
